24d6b33026e8790844fe6fbabbadffdfa34bf057
Kevin Zheng <kevinzhenng@gmail.com>
2020-01-10 10:53:40 -0500

    wip

--- internal/extsvc/bitbucketserver/events.go
+++ internal/extsvc/bitbucketserver/events.go
@@ -19,16 +19,29 @@ func ParseWebHook(event string, payload []byte) (interface{}, error) {
 	switch event {
 	case "pr:merged":
 		var mergeEvent PullRequestMergeEvent
-		return &mergeEvent, json.Unmarshal(payload, &mergeEvent)
+		return unmarshal(&mergeEvent, payload)
 	case "pr:declined":
 		var declinedEvent PullRequestDeclinedEvent
-		return &declinedEvent, json.Unmarshal(payload, &declinedEvent)
+		return unmarshal(&declinedEvent, payload)
 	case "pr:deleted":
 		var deletedEvent PullRequestDeletedEvent
-		return &deletedEvent, json.Unmarshal(payload, &deletedEvent)
+		return unmarshal(&deletedEvent, payload)
+	case "pr:comment:added":
+		var commentAddedEvent PullRequestCommentAddedEvent
+		return unmarshal(&commentAddedEvent, payload)
+	case "pr:comment:deleted":
+		var commentDeletedEvent PullRequestCommentDeletedEvent
+		return unmarshal(&commentDeletedEvent, payload)
+	case "pr:comment:edited":
+		var commentEditedEvent PullRequestCommentEditedEvent
+		return unmarshal(&commentEditedEvent, payload)
 	}
 
 	return nil, errors.New("unsupported event")
+}
+
+func unmarshal(e interface{}, payload []byte) (interface{}, error) {
+	return e, json.Unmarshal(payload, e)
 }
 
 type PullRequestEvent struct {
@@ -40,3 +53,16 @@ type PullRequestEvent struct {
 type PullRequestMergeEvent PullRequestEvent
 type PullRequestDeclinedEvent PullRequestEvent
 type PullRequestDeletedEvent PullRequestEvent
+
+type PullRequestCommentEvent struct {
+	PullRequestEvent
+	Comment Comment `json:"comment"`
+}
+
+type PullRequestCommentAddedEvent PullRequestCommentEvent
+type PullRequestCommentDeletedEvent PullRequestCommentEvent
+
+type PullRequestCommentEditedEvent struct {
+	PullRequestCommentEvent
+	PreviousText string `json:"previousText"`
+}
